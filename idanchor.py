4a33aaff1fa4dfb790add651689265bc2b344301
heffnercj <heffnercj@225fdc25-dc47-3c9e-ae55-7efed1beda56>
2011-09-26 01:59:43 +0000

    Modified to be an IDAPython script - much faster and more reliable.

--- idanchor.py
+++ idanchor.py
@@ -1,167 +1,148 @@
-#!/usr/bin/env python
+# IDAnchor - Generates hyperlinked HTML disassembly file.
+#
+# Craig Heffner
+# http://www.devttys0.com
+# September 25, 2011
 
-import os
+name_list = []
+ida_file_name = "out.html"
+html_file_name = "ida.html"
+nav_file_name = "nav.html"
+index_file_name = "index.html"
+count = 0
 
-class IDAnchor:
+# Get the area selected in the IDA screen (doesn't seem to work with 64 bit binaries?)
+start_ea = SelStart()
+end_ea = SelEnd()
 
-	SUBDELIM = "S U B R O U T I N E"
-	XREF_MARKER = "<span style=\"color:navy\">"
-	SUBNAME_MARKER = "<span style=\"color:navy\">:"
-	INDEX = '<html><frameset cols="20%%,80%%"><frame src="nav.html" /><frame name="ida" src="ida.html" /></frameset></html>'
+if start_ea == BADADDR or end_ea == BADADDR:
+        print "Please select an area to export!"
+        exit(1)
 
-	def __init__(self, infile, outdir):
-		self.xrefs = []
-		self.functions = []
-		self.outdir = outdir
-		self.html = open(infile).read().split('\n')
+# User must specify an output directory
+ddir = AskFile(1, "*", "Select output directory:")
+if ddir is None:
+        print "No output directory specified! Quitting..."
+        exit(1)
 
-	def PatchAll(self):
-		self.PatchXRefs()
-		self.PatchFunctions()
+# All output files go into the specified directory
+ida_file = "%s/%s" % (ddir, ida_file_name)
+html_file = "%s/%s" % (ddir, html_file_name)
+nav_file = "%s/%s" % (ddir, nav_file_name)
+index_file = "%s/%s" % (ddir, index_file_name)
 
-	def PatchXRefs(self):
-		self.FindXRefs()
-		self.LinkXRefs()
-		self.MarkXRefs()
-		return len(self.xrefs)
+# Make sure the directory exists
+if not os.path.exists(ddir):
+        print "Creating output directory %s..." % ddir
+        os.makedirs(ddir)
 
-	def PatchFunctions(self):
-		self.FindFunctions()
-		self.LinkFunctions()
-		self.MarkFunctions()
-		return len(self.functions)
+# Generate HTML file
+print "Generating HTML file (0x%X - 0x%X)..." % (start_ea, end_ea)
+n = GenerateFile(OFILE_LST, ida_file, start_ea, end_ea, GENFLG_GENHTML)
 
-	def FindFunctions(self):
-		sub = 0
-		num = 0
+if n < 1:
+        print "failed to generate HTML file %s!" % ida_file
+        exit(1)
+else:
+        print "%d lines of HTML generated OK." % n
 
-		for line in self.html:
+print "Searching for names...",
 
-			if not sub and self.SUBDELIM in line:
-				sub = True
-			elif self.SUBNAME_MARKER in line:
-				name = line.split('</span>')[1].split('<span')[0]
-				self.functions.append((num, name))
-			elif sub > 10:
-				sub = 0
-			else:
-				sub+=1
+# Get a list of all known functions and append the function address and name to the name_list array
+for ea in Functions():
+        if ea <= end_ea:
+                addr = "%8X" % ea
+                name = Name(ea)
+                name_list.append((addr.strip(), name))
+                count += 1
 
-			num+=1
+# Generate the function navigation file
+nfp = open(nav_file, "w")
+nfp.write("<html><body>\n")
+for (addr, name) in name_list:
+        nfp.write('<a target="ida" href="%s#%s">%s</a><br />\n' % (html_file_name, addr, name))
+nfp.write("</body></html>")
+nfp.close()
 
-		return num
+# Generate the main index file
+hfp = open(index_file, "w")
+hfp.write('<html><frameset cols="20%%,80%%"><frame src="nav.html" /><frame name="ida" src="ida.html" /></frameset></html>')
+hfp.close()
 
-	def FindXRefs(self):
-		num = 0
+# Loop through the entire selected area looking for named offsets that we don't already know about
+ea = start_ea
+while ea < end_ea:
 
-		for line in self.html:
-			
-			if line.startswith('.') and '# CODE XREF:' in line:
-				name = line.split(self.XREF_MARKER)[1].split(':')[0]
-				self.xrefs.append((num, name))
+        try:
+                # Only process code areas. If we hit a non-code area, stop searching for names.
+                if GetSegmentAttr(ea, SEGATTR_TYPE) != 0x02:
+                        break
+        except:
+                pass
 
-			num += 1
+        # Check if this offset has a name; if so, add its name and address to name_list.
+        name = Name(ea)
+        if name and name not in name_list:
+                addr = ("%X" % ea).strip()
+                name_list.append((addr, name))
+                Jump(ea)
+                count += 1
+        ea += 1
 
-		return num
+# Record where we stopped; we'll need this later
+end_code_ea = ea
 
-	def Mark(self, info):
-		count = 0
+print "found %d names." % count
 
-                for (line, name) in info:
-                        self.html[line] = self.html[line].replace('<span', '<span id="%s"' % name)
-                        count += 1
+print "Adding hyperlinks to HTML file...",
 
-                return count
+ifp = open(ida_file, "r")
+ofp = open(html_file, "w")
+inbody = False
 
-	def MarkFunctions(self):
-		return self.Mark(self.functions)
+# When parsing the HTML, names may be bracketed by these characters
+prefixes = [' ', '>', '(']
+postfixes = ['\r', '\n', ' ', '+', '<']
 
-	def MarkXRefs(self):
-		return self.Mark(self.xrefs)
+for line in ifp.readlines():
 
-	def Link(self, info):
-		prefixes = [' ', '>']
-		postfixes = ['\r', '\n', ' ', '+', '<']
+        if line.startswith("<body"):
+                #line = "%s<pre>" % line
+                inbody = True
+        elif "</body>" in line:
+                #line = line.replace("</body>", "</pre></body>")
+                inbody = False
+        elif inbody:
+                try:
+                        # Parse out the address of this line
+                        laddr = line.split('.')[1].split(':')[1].split(' ')[0]
+                        saddr = laddr.lstrip('0').strip()
+                        try:
+                                # Make sure we parsed a valid ea, else skip this part
+                                tea = int(saddr, 16)
+                                if tea >= start_ea and tea <= end_ea:
+                                        # Mark each line with its address
+                                        line = line.replace('<span', '<span id="%s"' % saddr, 1)
+                        except:
+                                pass
+                except:
+                        pass
 
-		for i in range(0, len(self.html)):
-			for (line, name) in info:
-				if name in self.html[i]:
-					for prefix in prefixes:
-						for postfix in postfixes:
-							search = '%s%s%s' % (prefix, name, postfix)
-							replace = '%s<a href="#%s">%s</a>%s' % (prefix, name, name, postfix)
+                # Find all references to all names in this line and wrap the name with a hyperlink to the named line
+                for (addr, name) in name_list:
 
-							self.html[i] = self.html[i].replace(search, replace)
-		return
+                        # Don't hyperlink names outside of the program's code area (externs, etc)
+                        if name in line and int(addr, 16) <= end_code_ea:
+                                for prefix in prefixes:
+                                        for postfix in postfixes:
+                                                search = '%s%s%s' % (prefix, name, postfix)
+                                                replace = '%s<a href="#%s">%s</a>%s' % (prefix, addr.strip(), name, postfix)
+                                                line = line.replace(search, replace)
+                
+        ofp.write(line)
 
-	def LinkFunctions(self):
-		return self.Link(self.functions)
+ifp.close()
+ofp.close()
+os.remove(ida_file)
 
-	def LinkXRefs(self):
-		return self.Link(self.xrefs)
-
-	def Save(self):
-		os.mkdir(self.outdir)
-		os.chdir(self.outdir)
-
-		self.WriteToFile()
-		self.GenNavBar()
-		self.GenIndexPage()
-
-	def WriteToFile(self):
-		fp = open("ida.html", "w")
-
-		for line in self.html:
-			fp.write("%s\n" % line)
-
-		fp.close()
-
-	def GenNavBar(self):
-		names = []
-
-		fp = open("nav.html", "w")		
-		fp.write("<html><body>")
-
-		for (line, name) in self.functions:
-			names.append(name)
-		names.sort()
-
-		for name in names:
-			fp.write('<a target="ida" href="ida.html#%s">%s</a><br />' % (name, name))
-
-		fp.write("</body></html>")
-		fp.close()
-
-	def GenIndexPage(self):
-		fp = open("index.html", "w")
-		fp.write(self.INDEX)
-		fp.close()
-
-
-if __name__ == "__main__":
-
-	import sys
-
-	if len(sys.argv) != 3:
-		print ""
-		print "Usage: %s <IDA html file> <output directory>" % sys.argv[0]
-		print ""
-		print "Example: %s idafile.html out" % sys.argv[0]
-		print ""
-		sys.exit(1)
-	
-	target = sys.argv[1]
-	outdir = sys.argv[2]
-
-	try:
-		a = IDAnchor(target, outdir)
-		print "Processing %s; this could take a while..." % target
-
-		xc = a.PatchXRefs()
-		fc = a.PatchFunctions()
-		a.Save()
-
-		print "Processed %d functions and %d code xrefs. Data saved to %s." % (fc, xc, outdir)
-	except Exception, e:
-		print "ERROR:", e
-
+print "Done."
